112c28bc1bbba3351e0ffac901e5cdde029c6380
hongliu-humor <hongmliu@ebay.com>
2025-02-07 15:08:42 +0800

    initialize project
    upload files

---
+++ hon-lib-core/src/main/java/com/hon/components/Answer20250207.java
@@ -0,0 +1,13 @@
+package com.hon.components;
+
+public class Answer20250207 {
+
+
+//    输入：n = 3
+//    输出：[[1,2,3],[8,9,4],[7,6,5]]
+    public int[][] generateMatrix(int n) {
+
+        int[][] matrix = new int[n][n];
+        return matrix;
+    }
+}
---
+++ hon-lib-core/src/main/java/com/hon/components/Hello.java
@@ -0,0 +1,7 @@
+package com.hon.components;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
